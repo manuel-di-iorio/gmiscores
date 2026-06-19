52d5c50b09f6304f632e882e3476f4c89cd7b9ea
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2026-06-19 23:50:00 +0200

    Revert "feat: add admin performance analysis page with query optimization and missing index detection features"
    This reverts commit fc853707dd77aafe6b3cffec2c1c8b95ad7f24b4.

--- includes/navbar.php
+++ includes/navbar.php
@@ -17,7 +17,6 @@ if ($isAdminUser) {
     $adminLabel .= ' <span class="nav-badge">' . $pendingCount . '</span>';
   }
   $navbarItems[] = ["label" => $adminLabel, "url" => "/admin.php", "icon" => "cogs", "showOnlyLogged" => true, "allowHtml" => true];
-  $navbarItems[] = ["label" => "Performance", "url" => "/admin-performance.php", "icon" => "tachometer-alt", "showOnlyLogged" => true];
 }
 
 $userTeamsList = [];
