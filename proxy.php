6fe893dfc92c1848c8f9e8a9e6cc9bb3680c5f38
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2026-06-29 13:36:07 +0200

    fix(proxy): ripristina la visualizzazione degli errori e rimuovi intestazioni non necessarie nel file proxy.php

--- proxy.php
+++ proxy.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $path = $_GET['path'] ?? '';
 
 if ($path === '' || strpos($path, 'api/') !== 0) {
@@ -13,8 +16,5 @@ if (!file_exists($file)) {
   http_response_code(404);
   exit;
 }
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
 
 include $file;
