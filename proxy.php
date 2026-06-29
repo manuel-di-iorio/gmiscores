4f58f99d4051a916c8da1d30944721f922516418
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2026-06-29 13:32:22 +0200

    fix(proxy): rimuovi i log di errore nel file proxy.php

--- proxy.php
+++ proxy.php
@@ -1,7 +1,7 @@
 <?php
 
 $path = $_GET['path'] ?? '';
-error_log("proxy.php called: path=" . $path . " method=" . $_SERVER["REQUEST_METHOD"]);
+$path = str_replace('\\', '/', $path);
 
 if ($path === '' || !preg_match('#^api/(v\d+)/(.+\.php)$/', $path)) {
   http_response_code(404);
@@ -10,7 +10,6 @@ if ($path === '' || !preg_match('#^api/(v\d+)/(.+\.php)$/', $path)) {
 }
 
 $file = __DIR__ . '/' . $path;
-error_log("proxy.php file=" . $file . " exists=" . (file_exists($file) ? 'yes' : 'no'));
 
 if (!file_exists($file)) {
   http_response_code(404);
