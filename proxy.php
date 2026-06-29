a37a480f0f5aeabbeb2169a50c46f68afc198dd1
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2026-06-29 13:33:46 +0200

    fix(proxy): migliora il messaggio di errore per il percorso non valido nel file proxy.php

--- proxy.php
+++ proxy.php
@@ -1,11 +1,17 @@
 <?php
 
 $path = $_GET['path'] ?? '';
+
+echo json_encode([
+  "raw" => $path,
+  "bytes" => array_map('ord', str_split($path)),
+]);
+
 $path = str_replace('\\', '/', $path);
 
 if ($path === '' || !preg_match('#^api/(v\d+)/(.+\.php)$/', $path)) {
   http_response_code(404);
-  echo json_encode(["debug" => "regex_fail", "path" => $path]);
+  echo json_encode(["debug" => "regex_fail", "path" => $path, "raw_path" => $_GET['path'] ?? 'empty']);
   exit;
 }
 
